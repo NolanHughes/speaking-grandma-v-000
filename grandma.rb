51db40e5db3ba9adf72ec058c21d08f4555a7ab1
Nolan Hughes <nhughes987@gmail.com>
2017-10-26 00:38:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  else   
+  else
     "HUH?! SPEAK UP, SONNY!"
   end
 
